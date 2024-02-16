8bdc257acd1bfef6a97d265fa479d81fad3b5ffe
Lim137 <sergei.ignatov2001@inbox.ru>
2024-02-16 09:46:18 +0300

    Server ready to work

---
+++ json.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failed to marshal JSON response: %v", payload)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	if code > 499 {
+		log.Printf("Responding with 5XX level error: %v", message)
+	}
+	type errRespond struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errRespond{Error: message})
+}
